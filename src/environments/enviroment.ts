73cb49aeeed8b0a1d0f0117799d096fbda2ad8e4
2211005 <144830831+2211005@users.noreply.github.com>
2024-12-01 17:46:13 -0500

    respaldo

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api', // URL del backend para desarrollo
+    apiUrl: 'backend-pctechnosystem-production.up.railway.app', // URL del backend para desarrollo
   };
   
