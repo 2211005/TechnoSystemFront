a35a1050bd30e970f5d2717b35a28f0a009e5119
2211005 <144830831+2211005@users.noreply.github.com>
2024-12-01 18:51:14 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://backend-pctechno-system-production.up.railway.app/api', // URL del backend para producción
+    apiUrl: 'https://backend-pctechnosystem-production.up.railway.app/api', // URL del backend para producción
   };
   
